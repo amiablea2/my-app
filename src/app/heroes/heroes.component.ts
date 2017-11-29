8cb4de37c2fbfe0ff6c7389561db5623b6264fcc
Kaushal <kaushal.kishore@gmail.com>
2017-11-29 15:25:31 +0530

    4. Master/Detail Components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,6 +20,11 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
 
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
   constructor() { }
 
   ngOnInit() {
